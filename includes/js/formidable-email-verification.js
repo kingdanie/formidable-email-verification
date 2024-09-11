544bc06f554d052f30be956d324ae374efdb8822
kingdanie <daniedemola@gmail.com>
2024-09-11 02:46:01 +0100

    1. add spinner to submit and verify buttons 2. made validation msg temporary 3. imprive margin btw elements

--- includes/js/formidable-email-verification.js
+++ includes/js/formidable-email-verification.js
@@ -4,7 +4,10 @@ jQuery(document).ready(function ($) {
         if (isValidationRunning) return;
 
         isValidationRunning = true;
-		$('#send_otp_button').prop('disabled', true);
+		// $('#send_otp_button').prop('disabled', true);
+        let originalText = $(this).html();
+        $('#send_otp_button').prop('disabled', true).html('<span class="loader"></span> Sending...'); // Show spinner
+
 
         let email = $('#email_for_otp').val();
         $.ajax({
@@ -17,26 +20,38 @@ jQuery(document).ready(function ($) {
             },
             success: function (response) {
                 if (response.success) {
-                    $('#otp_message').text(response.data.message);
+                    // $('#otp_message').text(response.data.message);
+                    $('#otp_message').text(response.data.message).removeClass('error-message').addClass('success-message').show();
                     $('#otp_input_wrapper').show();
 //                     $('#email_for_otp').val(email);
                     $('#send_otp_button').hide();
 					
                 } else {
-                    $('#otp_message').text(response.data.message);
+                    // $('#otp_message').text(response.data.message);
+                    $('#otp_message').text(response.data.message).removeClass('success-message').addClass('error-message').show();
 					$('#send_otp_button').prop('disabled', false);
                 }
+                
+                 // Hide the message after 10 seconds
+                 setTimeout(function () {
+                    $('#otp_message').fadeOut(); // Or you can use `.hide()` to remove instantly
+                }, 7000); // 10000 milliseconds = 10 seconds
             },
             complete: function () {
                 isValidationRunning = false;
+                $('#send_otp_button').prop('disabled', false).html(originalText); 
             }
         });
     });
 
     $('#verify_otp_button').on('click', function () {
+        let originalText = $(this).html(); // Save the original button text
+        $('#verify_otp_button').prop('disabled', true).html('<span class="loader"></span> Verifying...'); // Show spinner
+
+
         let email_for_otp = $('#email_for_otp').val();
         let otp = $('#otp_input').val();
-		$('#verify_otp_button').prop('disabled', true);
+		// $('#verify_otp_button').prop('disabled', true);
         $.ajax({
             url: fev_ajax.ajax_url,
             type: 'POST',
@@ -48,7 +63,8 @@ jQuery(document).ready(function ($) {
             },
             success: function (response) {
                 if (response.success) {
-                    $('#otp_message').text(response.data.message);
+                    // $('#otp_message').text(response.data.message);
+                    $('#otp_message').text(response.data.message).removeClass('error-message').addClass('success-message').show();
                     $('#verified_email').val(email_for_otp);
 //                     $('#email_for_otp').val(email_for_otp);
                     $('#otp_verification_status').val('1');
@@ -62,12 +78,20 @@ jQuery(document).ready(function ($) {
 					$('#send_otp_button').remove();
                    
                 } else {
-                    $('#otp_message').text(response.data.message);
+                    // $('#otp_message').text(response.data.message);
+                    $('#otp_message').text(response.data.message).removeClass('success-message').addClass('error-message').show();
                     $('#send_otp_button').show();
                     $('#otp_input_wrapper').hide();
 					$('#verify_otp_button').hide();
 					$('#send_otp_button').prop('disabled', false);
                 }
+                 // Hide the message after 10 seconds
+                 setTimeout(function () {
+                    $('#otp_message').fadeOut(); // Or you can use `.hide()` to remove instantly
+                }, 7000); // 10000 milliseconds = 10 seconds
+            },
+            complete: function () {
+                // $('#verify_otp_button').prop('disabled', false).html(originalText); // Restore original text
             }
         });
     });
